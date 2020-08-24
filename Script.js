f97042ff6ca840d6297e79acd14692dcc6ee8672
Aniket Das <64518145+anik8das@users.noreply.github.com>
2020-08-24 18:20:29 +0530

    Improved UI and more JS functionality

--- Script.js
+++ Script.js
@@ -1,7 +1,18 @@
-var busy = [];
+var monbusy = [];
+var tuebusy = [];
+var wedbusy = [];
+var thubusy = [];
+var fribusy = [];
+var satbusy = [];
+var sunbusy = [];
 var task = [];
 var busynum = 3;
 var tasknum = 3;
+var busy = [];
+
+function sortarr(a, b) {
+    return a[0] - b[0];
+}
 
 function addbusy() {
     var name = document.getElementById("workinput").value;
@@ -29,13 +40,82 @@ function addbusy() {
     document.getElementById('timeinputfrom').value = '';
     document.getElementById('timeinputto').value = '';
     document.getElementById('freqinput').value = '';
+    console.log(freq.toUpperCase());
+    if (freq.toUpperCase() == "DAILY") {
+        monbusy.push([timefrom, timeto]);
+        monbusy.sort(sortarr);
+        tuebusy.push([timefrom, timeto]);
+        tuebusy.sort(sortarr);
+        wedbusy.push([timefrom, timeto]);
+        wedbusy.sort(sortarr);
+        thubusy.push([timefrom, timeto]);
+        thubusy.sort(sortarr);
+        fribusy.push([timefrom, timeto]);
+        fribusy.sort(sortarr);
+        satbusy.push([timefrom, timeto]);
+        satbusy.sort(sortarr);
+        sunbusy.push([timefrom, timeto]);
+        sunbusy.sort(sortarr);
+    } else {
+        freq = freq.split(",");
+        console.log("freq:", freq);
+        for (let item of freq) {
+            item = item.trim();
+            console.log("item:", item);
+            switch (item.toUpperCase()) {
+                case "MONDAY":
+                    {
+                        monbusy.push([timefrom, timeto]);
+                        monbusy.sort(sortarr);
+                        break;
+
+                    }
+                case "TUESDAY":
+                    {
+                        tuebusy.push([timefrom, timeto]);
+                        tuebusy.sort(sortarr);
+                        break;
+                    }
+                case "WEDNESDAY":
+                    {
+                        wedbusy.push([timefrom, timeto]);
+                        wedbusy.sort(sortarr);
+                        break;
+                    }
+                case "THURSDAY":
+                    {
+                        thubusy.push([timefrom, timeto]);
+                        thubusy.sort(sortarr);
+                        break;
+                    }
+                case "FRIDAY":
+                    {
+                        fribusy.push([timefrom, timeto]);
+                        fribusy.sort(sortarr);
+                        break;
+                    }
+                case "SATURDAY":
+                    {
+                        satbusy.push([timefrom, timeto]);
+                        satbusy.sort(sortarr);
+                        break;
+                    }
+                case "SUNDAY":
+                    {
+                        sunbusy.push([timefrom, timeto]);
+                        sunbusy.sort(sortarr);
+                        break;
+                    }
+            }
+        }
+    }
+
 }
 
 function addtask() {
     var name = document.getElementById("taskname").value;
     var time = document.getElementById("tasktime").value;
     var daily = document.getElementById("taskdaily").value;
-    var spaced = document.getElementById("taskspaced");
     var freq = document.getElementById("taskfrequency").value;
     var tasktable = document.getElementById("tasktable");
     var row = tasktable.insertRow(tasknum);
@@ -43,21 +123,14 @@ function addtask() {
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
-    var cell5 = row.insertCell(4);
     cell1.innerHTML = name;
     cell2.innerHTML = time;
     cell3.innerHTML = daily;
-    if (spaced.checked == true) {
-        cell4.innerHTML = "&#9745";
-    } else {
-        cell4.innerHTML = "&#9744"
-    }
-    cell5.innerHTML = freq;
-    task.push([name, time, daily, spaced, freq]);
+    cell4.innerHTML = freq;
+    task.push([name, time, daily, freq]);
     document.getElementById("taskname").value = "";
     document.getElementById("tasktime").value = "";
     document.getElementById("taskdaily").value = "";
-    spaced.checked = false;
     document.getElementById("taskfrequency").value = "";
     tasknum++;
     console.log(task);
@@ -78,5 +151,5 @@ function removebusy() {
 }
 
 function makeschedule() {
-    var dailytasktime = [0, 0, 0, 0, 0, 0, 0];
+    console.log(monbusy);
 }
